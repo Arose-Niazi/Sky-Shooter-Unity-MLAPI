4242732a4a55fb0dcb466838567246ef97881258
Arose Niazi <arose.niazi@yahoo.com>
2021-04-17 22:24:11 +0500

    Connection Panel Completed.

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,11 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using MLAPI;
+using MLAPI.Transports.UNET;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
+    public GameObject ipPanel;
+    public InputField ipField;
     public void Play()
     {
         SceneManager.LoadScene("Loading");
@@ -22,9 +27,40 @@ public class MainMenu : MonoBehaviour
         //SceneManager.LoadScene("Hosting");
     }
     
-    public void Client()
+    public void Join()
     {
+        ipPanel.SetActive(true);
+    }
+
+    public void Connect()
+    {
+        if(!ValidateIPv4(ipField.text)) return;
+        NetworkManager.Singleton.GetComponent<UNetTransport>().ConnectAddress = ipField.text;
         NetworkManager.Singleton.StartClient();
     }
+
+    public void Back()
+    {
+        ipPanel.SetActive(false);
+    }
+    
+    public bool ValidateIPv4(string ipString)
+    {
+        if (string.IsNullOrWhiteSpace(ipString))
+        {
+            return false;
+        }
+
+        string[] splitValues = ipString.Split('.');
+        if (splitValues.Length != 4)
+        {
+            return false;
+        }
+
+        byte tempForParsing;
+
+        return splitValues.All(r => byte.TryParse(r, out tempForParsing));
+    }
+
 
 }
